903e7f4d72f001f80c0e110300a0fae45cf50f00
Newguidance1 <newguidance4141@gmail.com>
2025-04-11 08:08:21 -0700

    added register and login

--- src/main.jsx
+++ src/main.jsx
@@ -14,8 +14,8 @@ import Register from './pages/Register.jsx'
 
 
 const router = createBrowserRouter([
-  { path: '/', element: <Login /> },
-  { path: '/register', element: <Register /> },
+  // { path: '/home', element: <Login /> },
+  // { path: '/register', element: <Register /> },
 
   
   { path: '/home', element: <App />,
